b6d7932b397bddff5584adf66487719b6a589254
Vodopadiks <vsamsonovitch@gmail.com>
2023-07-07 16:09:44 +0300

    Adding git tag

--- build/git_update.sh
+++ build/git_update.sh
@@ -59,6 +59,7 @@ if [ -z "$NEEDS_TAG" ]; then
     echo "Tagged with $NEW_TAG"
     git tag $NEW_TAG
     echo "Premission Denied  ???"
+    git remote -v
     git push --tags -u origin $ACCESS_TOKEN
 
     git push -u $ACCESS_TOKEN
