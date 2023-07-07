04f6c648bfaf3e9a4b51a4ef8895fde3fcb18cfe
Vodopadiks <vsamsonovitch@gmail.com>
2023-07-07 16:25:26 +0300

    Adding git tag

--- build/git_update.sh
+++ build/git_update.sh
@@ -63,7 +63,7 @@ if [ -z "$NEEDS_TAG" ]; then
     git remote set-url origin git@github.com:Vodopadiks/view_repo.git
     git remote -v
     ssh-add -l
-    ssh-add ~/.ssh/ssh_git
+    ssh-add /secrets/ssh_git
     ssh -T git@github.com
     eval "$(ssh-agent -k)"
     eval "$(ssh-agent -s)"
